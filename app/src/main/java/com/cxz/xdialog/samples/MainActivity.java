a09fdbc28c149232004faebdaa2ecafa6d4f5355
chenxz <563859095@qq.com>
2018-09-10 15:58:04 +0800

    commit

--- app/src/main/java/com/cxz/xdialog/samples/MainActivity.java
+++ app/src/main/java/com/cxz/xdialog/samples/MainActivity.java
@@ -1,13 +1,26 @@
 package com.cxz.xdialog.samples;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.cxz.xdialog.samples.bottomdialog.ShareBottomDialog;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_bottom_dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_bottom_dialog = findViewById(R.id.btn_share_bottom);
+        btn_bottom_dialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new ShareBottomDialog().show(getSupportFragmentManager());
+            }
+        });
     }
 }
