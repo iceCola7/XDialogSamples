797e6c521b87522e15b935bcd9a4a4bd8455e7b0
chenxz <563859095@qq.com>
2018-09-10 16:27:26 +0800

    mod

--- app/src/main/java/com/cxz/xdialog/samples/MainActivity.java
+++ app/src/main/java/com/cxz/xdialog/samples/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.cxz.xdialog.samples.bottomdialog.EditBottomDialog;
 import com.cxz.xdialog.samples.bottomdialog.ShareBottomDialog;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +18,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 new ShareBottomDialog().show(getSupportFragmentManager());
+            }
+        });
+
+        findViewById(R.id.btn_edit_bottom).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new EditBottomDialog().show(getSupportFragmentManager());
             }
         });
 
